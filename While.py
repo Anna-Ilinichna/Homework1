7df73b1d1315813930aeab33bd5d3cae53837abc
Anna-Ilinichna <57904879+Anna-Ilinichna@users.noreply.github.com>
2019-12-01 14:42:43 +0300

    Delete While.py

--- While.py
+++
@@ -1,7 +0,0 @@
-user_say=input('Как дела?:')
-user_ask=input('Что делашь?:')
-while True:
-    if user_say == 'Хорошо' and user_ask == 'Программирую':
-        print('Отлично')     
-    else:   
-        print('Пока!')
