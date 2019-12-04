c330075d82c538502a5b51cf5358dcf8af6c8037
Anna-Ilinichna <57904879+Anna-Ilinichna@users.noreply.github.com>
2019-12-04 16:17:46 +0300

    Update ephem_bot.py

--- ephem_bot.py
+++ ephem_bot.py
@@ -10,13 +10,16 @@ logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     )
 import ephem
 
+from datetime import datetime
+dt_now = datetime.now()
+
 def main():
     mybot = Updater("998165570:AAHwOKmYmf-86VvkMr-MQL4PfHl29N0RP80", request_kwargs=PROXY)
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
-    # dp.add_handler(MessageHandler(Filters.text, talk_to_me))
-    dp.add_handler(MessageHandler(Filters.text, planet_text))
-    
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    dp.add_handler(CommandHandler('planet', planet_text))
+     
     mybot.start_polling()
     mybot.idle()
 
@@ -26,17 +29,16 @@ def greet_user(bot, update):
     print(text)
     update.message.reply_text(text)
 
-# def talk_to_me(bot, update):
-#     user_text = update.message.text 
-#     print(user_text)
-#     update.message.reply_text(user_text)
-        
 def planet_text(bot, update):
-    user_planet=update.message.text.split()
-    update.message.reply_text(user_planet)
-    planet_text == getattr(ephem, user_planet('2000/01/01'))
-    constellation = ephem.constellation(planet_text)
+    user_planet = update.message.text.split()
+    planet = getattr(ephem, user_planet(dt_now))
+    constellation = ephem.constellation(planet)
     print(constellation)
     update.message.reply_text(constellation)
-    
-main()
+
+def talk_to_me(bot, update):
+    user_text = update.message.text
+    print(user_text)
+    update.message.reply_text(user_text)
+       
+main()  
